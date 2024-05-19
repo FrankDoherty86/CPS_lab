f2af801e3de631bf0932155a06c9de7ff0bcc36f
FrankDoherty86 <fdoherty86@gmail.com>
2024-05-19 21:31:58 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include "mbed.h"
+#include <cstdio>
+
+int t1 = 0;
+int t2 = 1;
+int next_term = t1 + t2;
+int max_terms = 8;
+
+int main()
+{
+printf("Hello, lab program 1 \r\n");
+printf("Fibonacchi sequence: %d, %d, ", t1, t2);
+
+for(int i=3; i<max_terms; i++){
+    printf("%d, ", next_term);
+    t1 = t2;
+    t2 = next_term;
+    next_term = t1 + t2;
+}
 
 // main() runs in its own thread in the OS
-int main()
+
 {
     while (true) {
 
     }
 }
 
+}
